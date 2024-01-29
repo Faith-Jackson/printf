5e594876ffe89c22ad58af56317156f6612be290
Faith-Jackson <undisputedfaith@gmail.com>
2024-01-29 19:17:37 +0100

    working

--- _printf.c
+++ _printf.c
@@ -2,72 +2,65 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/**
- * _printf - Produces output according to a format.
- * @format: Character string containing zero or more directives.
- *
- * Return: The number of characters printed (excluding the null byte).
- */
 int _printf(const char *format, ...)
 {
     va_list args;
-    int count = 0;
-    const char *ptr;
+    int count;
 
     if (!format)
         return -1;
 
     va_start(args, format);
 
-    ptr = format;
-
-    while (*ptr)
+    count = 0;
+    while (*format)
     {
-        if (*ptr == '%')
+        if (*format != '%')
         {
-            ptr++; /* Move past '%' */
+            count += write(1, format, 1);
+        }
+        else
+        {
+            format++; /* Move past '%' */
 
-            if (*ptr == '\0')
+            if (*format == '\0')
             {
-                /* Handle incomplete format string */
-                count += write(1, "%", 1);
+                count = -1; /* Incomplete format string */
                 break;
             }
 
-            if (*ptr == 'c')
+            if (*format == 'c')
             {
-                /* Handle character specifier */
                 char c = va_arg(args, int);
                 count += write(1, &c, 1);
             }
-            else if (*ptr == 's')
+            else if (*format == 's')
             {
-                /* Handle string specifier */
                 char *s = va_arg(args, char *);
-                int len = 0;
-                while (s[len])
-                    len++;
-                count += write(1, s, len);
+                if (s)
+                {
+                    int len = 0;
+                    while (s[len])
+                        len++;
+                    count += write(1, s, len);
+                }
+                else
+                {
+                    count += write(1, "(null)", 6);
+                }
             }
-            else if (*ptr == '%')
+            else if (*format == '%')
             {
-                /* Handle percent specifier */
                 count += write(1, "%", 1);
             }
             else
             {
                 /* Invalid specifier, treat as literal '%' */
                 count += write(1, "%", 1);
-                count += write(1, ptr, 1);
+                count += write(1, format, 1);
             }
         }
-        else
-        {
-            /* Write character to stdout */
-            count += write(1, ptr, 1);
-        }
-
-        ptr++;
+        format++;
     }
 
     va_end(args);
